2ecfb60c2b8de27cd504a14bab649d0d08756ffc
Nathan Du <nathandu@outlook.com>
2021-03-29 20:51:19 +0800

    prevent downscaling of HD files if SD is up-to-date

--- downscale.ps1
+++ downscale.ps1
@@ -1,4 +1,16 @@
-ls -Filter "*@2x.png" | foreach {
+# Mass downscale HD osu! skin files to SD in the current directory.
+
+foreach ($_ in (ls -Filter "*@2x.png")) {
+    $downscaledFileName = ($_.Name.Substring(0, $_.Name.IndexOf("@"))+".png")
+    if (Test-Path $downscaledFileName) {
+        $downscaledFile = Get-Item -Path $downscaledFileName
+
+        if ($downscaledFile.LastWriteTime -gt $_.LastWriteTime) {
+            echo ($downscaledFile.Name + " is newer -- skipping")
+            continue
+        }
+    }
+
     echo ("Downscaling " + $_.Name)
-    magick $_.Name -resize "50%" ($_.Name.Substring(0, $_.Name.IndexOf("@"))+".png")
+    magick $_.Name -resize "50%" $downscaledFileName
 }
